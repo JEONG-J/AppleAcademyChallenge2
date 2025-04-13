1babce5d8e86de8d3b87e788299e9bcb45edc045
AppleDeveloper <ikejhc159@gmail.com>
2025-04-13 23:36:35 +0900

    ✨ [Feat] AppFlow 의존성 추가
    닉네임 존재 유무로 AppFlow 흐름 작성

--- FeelFarm/FeelFarm/App/AppFlowViewModel.swift
+++ FeelFarm/FeelFarm/App/AppFlowViewModel.swift
@@ -6,14 +6,43 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class AppFlowViewModel: ObservableObject {
     
-    @Published private var appState: AppState = .onboarding
+    @Published var appState: AppState = .onboarding
     
     enum AppState {
         case onboarding
         case createProfile
         case tabbar
+    }
+    
+    func checkAuthState() {
+        if let user = Auth.auth().currentUser {
+            print("자동 로그인된 사용자 UID: \(user.uid)")
+            checkUserProfile(uid: user.uid)
+        }
+    }
+    
+    func checkUserProfile(uid: String) {
+        let userRef = Firestore.firestore().collection("users").document(uid)
+        
+        userRef.getDocument { snapshot, error in
+            if let error = error {
+                print("사용자 문서 조회 실패: \(error)")
+                self.appState = .createProfile
+                return
+            }
+            
+            if let data = snapshot?.data(), data["nickname"] != nil {
+                print("닉네임이 등록된 사용자 -> 메인 이동")
+                self.appState = .tabbar
+            } else {
+                print("닉네임 정보 없음 -> 프로필 생성")
+                self.appState = .createProfile
+            }
+        }
     }
 }
